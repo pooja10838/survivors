2099c3cbc35aa5337955f997587c787b9bcdb4c2
ktheoxaroulis <ktheoxaroulis@gmail.com>
2020-04-10 00:31:27 +0300

    test

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from vega_datasets import data
 import streamlit as st
 import altair as alt
-
+from PIL import Image
 def main():
     df = load_data()
     page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
+    image = Image.open("images/image.png")
 
     if page == "Homepage":
-        st.header("This is your data explorer.")
+        st.header("Covid-19 Recovered Data")
         st.write("Please select a page on the left.")
         st.write(df)
     elif page == "Exploration":
         st.title("Data Exploration")
+        st.image(image, width =100)
         x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
         y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
         visualize_data(df, x_axis, y_axis)
